35b59d7e92be471b0ca50bbb208b6913b6f48c9d
DEVLlN <devlin.business@gmail.com>
2026-04-30 12:12:42 -0400

    Update Helm formula to 0.1.9

--- Formula/helm.rb
+++ Formula/helm.rb
@@ -1,9 +1,8 @@
 class Helm < Formula
-  desc "Helm bridge installer and runtime helpers"
+  desc "Bridge installer and runtime helpers"
   homepage "https://github.com/DEVLlN/helm"
   url "https://github.com/DEVLlN/helm/archive/refs/tags/v0.1.9.tar.gz"
   sha256 "a69f32e6b0a61c224a085c0d646522d71b7cd8d3578778878bda78ef5ed74bc8"
-  version "0.1.9"
   license :cannot_represent
   depends_on "node"
 
